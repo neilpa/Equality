570ba5c271346eb497fff0108b1c223df5d50639
Neil Pankey <npankey@gmail.com>
2015-03-14 13:02:13 -0700

    Initial commit

---
+++ Equality/Equality.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Equality.
+extern double EqualityVersionNumber;
+
+/// Project version string for Equality.
+extern const unsigned char EqualityVersionString[];
+

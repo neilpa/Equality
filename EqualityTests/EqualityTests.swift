fd879213a585023eec2dba00eea36782974a4745
Neil Pankey <npankey@gmail.com>
2015-03-14 13:40:43 -0700

    Commutativity tests

--- EqualityTests/EqualityTests.swift
+++ EqualityTests/EqualityTests.swift
@@ -7,9 +7,23 @@ final class EqualityTests: XCTestCase {
     func testEmpty() {
         XCTAssert(EmptySequence<Int>() == [])
         XCTAssert(Set<Character>() == [])
-
         XCTAssert([] == EmptySequence<Box<Bool>>())
-        XCTAssert([] == Set<String>())
+        XCTAssert(EmptySequence() == Set<String>())
+        XCTAssert(EmptySequence() == TestSequence<Int>())
+    }
+
+    func testCommutativity() {
+        XCTAssert(Set([42]) == TestSequence(42))
+        XCTAssert(TestSequence(42) == Set([42]))
+
+        XCTAssert(["foo", "bar", "baz"] == TestSequence("foo", "bar", "baz"))
+        XCTAssert(TestSequence("foo", "bar", "baz") == ["foo", "bar", "baz"])
+
+        XCTAssert("Hello" == TestSequence("H", "e", "l", "l", "o"))
+        XCTAssert(TestSequence<Character>("H", "e", "l", "l", "o") == "Hello")
+
+        XCTAssert(TestSequence(Box(1)) == [Box(1)])
+        XCTAssert([Box(1)] == TestSequence(Box(1)))
     }
 }
 
@@ -29,5 +43,16 @@ func == <T>(lhs: Box<T>, rhs: Box<T>) -> Bool {
 struct EmptySequence<T> : SequenceType {
     func generate() -> GeneratorOf<T> {
         return GeneratorOf { nil }
+    }
+}
+
+struct TestSequence<T> : SequenceType {
+    var values: [T]
+    init(_ values: T...) {
+        self.values = values
+    }
+
+    func generate() -> Array<T>.Generator {
+        return values.generate()
     }
 }

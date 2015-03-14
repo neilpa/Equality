2f8eacb62f1b287f4c0a4a86261a8d22d30e353e
Neil Pankey <npankey@gmail.com>
2015-03-14 14:01:52 -0700

    Cleanup naming

--- Equality/Equality.swift
+++ Equality/Equality.swift
@@ -1,26 +1,27 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 
-/// Defines an equality operator for arbitrary sequences of equatable elements
+/// Defines an equality operator for arbitrary sequences of equatable elements.
 ///
 /// In practice this is most useful in tests for asserting equality of custom sequences against some reference collection (e.g. an array).
 ///
-///     assert(MyCustomCollection(1, 2, 3) == [1, 2, 3])
-public func == <S0: SequenceType, S1: SequenceType, E: Equatable where S0.Generator.Element == E, S1.Generator.Element == E> (seq0: S0, seq1: S1) -> Bool {
-    var gen0 = seq0.generate()
-    var gen1 = seq1.generate()
+///     assert(CustomCollection(1, 2, 3) == [1, 2, 3])
+public func == <S0: SequenceType, S1: SequenceType, E: Equatable where S0.Generator.Element == E, S1.Generator.Element == E> (lhs: S0, rhs: S1) -> Bool {
+    var leftGenerator = lhs.generate()
+    var rightGenerator = rhs.generate()
 
-    var elem0 = gen0.next()
-    var elem1 = gen1.next()
+    var leftElement = leftGenerator.next()
+    var rightElement = rightGenerator.next()
 
-    while let val0 = elem0, let val1 = elem1 where val0 == val1 {
-        elem0 = gen0.next()
-        elem1 = gen1.next()
+    while let leftValue = leftElement, let rightValue = rightElement
+        where rightValue == leftValue {
+        leftElement = leftGenerator.next()
+        rightElement = rightGenerator.next()
     }
 
-    return elem0 == nil && elem1 == nil;
+    return leftElement == nil && rightElement == nil;
 }
 
 /// Defines an inequality operator for arbitrary sequences of equatable elements
-public func != <S0: SequenceType, S1: SequenceType, E: Equatable where S0.Generator.Element == E, S1.Generator.Element == E> (seq0: S0, seq1: S1) -> Bool {
-    return !(seq0 == seq1)
+public func != <S0: SequenceType, S1: SequenceType, E: Equatable where S0.Generator.Element == E, S1.Generator.Element == E> (lhs: S0, rhs: S1) -> Bool {
+    return !(lhs == rhs)
 }
